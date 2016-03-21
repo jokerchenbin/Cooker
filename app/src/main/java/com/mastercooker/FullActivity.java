26f273ac129a147b23a7ff023064464f6d10f5ac
tanghaibo111 <979095961@qq.com>
2016-03-21 18:50:10 +0800

    change class

--- app/src/main/java/com/mastercooker/FullActivity.java
+++ app/src/main/java/com/mastercooker/FullActivity.java
@@ -10,7 +10,7 @@ public class FullActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//change
         setContentView(R.layout.activity_full);
 
         new Thread() {
