675effa5d0d5577ea346ed90cf1636033a8452f3
bin_chen <bin_chen@loongjoy.com>
2016-03-21 17:50:53 +0800

    测试提交

--- app/src/main/java/com/mastercooker/MainActivity.java
+++ app/src/main/java/com/mastercooker/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//测试提交
         /*
         mToolbar = (Toolbar)findViewById(R.id.activity_main_tb);
         mToolbar.addView(view);
